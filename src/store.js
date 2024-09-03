e4921ae6ba4c6cb7c84524e3dd7aa9854956e8fc
madyanes <madyanes.dev@gmail.com>
2024-09-03 18:58:23 +0700

    Adding Customer state

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 }
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+}
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -42,7 +48,33 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer)
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      }
+
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payload,
+      }
+
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  custome: customerReducer,
+})
+
+const store = createStore(rootReducer)
 
 // Action creators ==> They're just simply functions that return an action. Not a must, just a convention.
 function deposit(amount) {
@@ -71,4 +103,26 @@ store.dispatch(deposit(500))
 store.dispatch(withdraw(200))
 store.dispatch(requestLoan(1000, 'Buy a cheap car'))
 store.dispatch(payLoan())
+console.log(store.getState())
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  }
+}
+
+function updateName(fullName) {
+  return {
+    type: 'customer/updateName',
+    payload: fullName,
+  }
+}
+
+store.dispatch(createCustomer('Madyan Eka Septian', '1234567'))
+store.dispatch(deposit(100))
 console.log(store.getState())
