8cb3092c2a2f7cccc9ec4993abdfc4d0cc4856e9
madyanes <madyanes.dev@gmail.com>
2024-09-03 15:45:24 +0700

    Create a Redux store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -20,10 +22,11 @@ function reducer(state = initialState, action) {
 
     case 'account/requestLoan':
       if (state.loan > 0) return state
-      // more code later
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       }
 
     case 'account/payLoan':
@@ -38,3 +41,21 @@ function reducer(state = initialState, action) {
       return state
   }
 }
+
+const store = createStore(reducer)
+
+store.dispatch({ type: 'account/deposit', payload: 500 })
+store.dispatch({ type: 'account/withdraw', payload: 200 })
+console.log(store.getState())
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: {
+    amount: 1000,
+    purpose: 'Buy a car',
+  },
+})
+console.log(store.getState())
+
+store.dispatch({ type: 'account/payLoan' })
+console.log(store.getState())
