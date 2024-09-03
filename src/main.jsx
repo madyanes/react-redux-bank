5f15484f4f6fb94bc64b823fc0978c5b1a232810
madyanes <madyanes.dev@gmail.com>
2024-09-03 19:24:53 +0700

    Professional Redux file structure: State Slices

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import './store'
+import store from './store'
+
+// Example
+store.dispatch({ type: 'account/deposit', payload: 777 })
+console.log(store.getState())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
